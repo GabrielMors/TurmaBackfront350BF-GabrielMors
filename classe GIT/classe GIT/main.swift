694b0f2e5372e5394996fbf395324166e09c122d
Gabriel Mors <gabrielmors210@gmail.com>
2023-03-07 19:46:50 -0400

    criando mais 3 classes e colocando 5 propriedades e 2 funcoes em cada uma

--- classe GIT/classe GIT/main.swift
+++ classe GIT/classe GIT/main.swift
@@ -30,3 +30,65 @@ class Automovel {
     }
 }
 
+//=========================================
+
+
+class Cavalo {
+    
+    var nome: String = "Rex"
+    var idade: Int = 10
+    var peso: Double = 300.0
+    var velocidadeMaxima: Int = 60
+    var raça: String = "Shire"
+    
+    func andar() {
+        print("O cavalo esta andando")
+    }
+    
+    func correr() {
+        print("O cavalo esta correndo")
+    }
+    
+}
+
+//=========================================
+
+class Casa {
+    var janela: Bool = true
+    var quantidadePortas: Int = 9
+    var quantidadeBanheiro: Int = 4
+    var temPiscina: Bool = true
+    var quantidadeQuartos: Int = 5
+    
+    
+    func abrirJanela() {
+        print("a janela esta aberta")
+    }
+    
+    func reforma() {
+        print("A casa esta em reforma")
+    }
+}
+
+
+//=========================================
+
+
+class humano {
+    var nome: String = "Gabriel"
+    var idade: Int = 18
+    var boca: Bool = true
+    var idioma: String = "Ingles"
+    var temBraco: Bool = true
+    
+    
+    func falar() {
+        print("O Gabriel está falando")
+    }
+    
+    func dormir() {
+        print("O Gabriel esta dormindo")
+    }
+    
+}
+
