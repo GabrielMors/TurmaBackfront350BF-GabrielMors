dd81717d0739eb6a9224e59db8d8c7189c08ada7
Gabriel Mors <gabrielmors210@gmail.com>
2023-03-23 10:59:11 -0400

    setei algumas logicas para o password, nao deu certo ainda

--- Desafio02TextField/Desafio02TextField/ViewController.swift
+++ Desafio02TextField/Desafio02TextField/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
         configEndereco()
         configPassword()
         configConfirmPassword()
+        configEntrarButton()
         nameTextField.delegate = self
         enderecoTextField.delegate = self
         passwordTextField.delegate = self
@@ -30,21 +31,31 @@ class ViewController: UIViewController {
 
     private func configName() {
         nameTextField.placeholder = "Digite seu nome:"
+        nameTextField.layer.borderWidth = 1
+        nameTextField.layer.borderColor = UIColor.lightGray.cgColor
         nameTextField.autocapitalizationType = .words
     }
 
     private func configEndereco() {
         enderecoTextField.placeholder = "Digite seu endereço:"
+        enderecoTextField.layer.borderWidth = 1
+        enderecoTextField.layer.borderColor = UIColor.lightGray.cgColor
         enderecoTextField.autocorrectionType = .no
     }
     
     private func configPassword() {
         passwordTextField.placeholder = "Digite sua senha:"
+        passwordTextField.layer.borderWidth = 1
+        passwordTextField.layer.borderColor = UIColor.lightGray.cgColor
+        passwordTextField.autocorrectionType = .no
 //        passwordTextField.isSecureTextEntry = true
     }
     
     private func configConfirmPassword() {
         confirmPasswordTextField.placeholder = "Confirme sua senha:"
+        confirmPasswordTextField.layer.borderWidth = 1
+        confirmPasswordTextField.layer.borderColor = UIColor.lightGray.cgColor
+        confirmPasswordTextField.autocorrectionType = .no
 //        confirmPasswordTextField.isSecureTextEntry = true
     }
     
@@ -62,6 +73,18 @@ extension ViewController: UITextFieldDelegate {
     }
     
     func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        if  let password = passwordTextField.text,
+            let confirmPassword = confirmPasswordTextField.text,
+            confirmPassword.caseInsensitiveCompare(password) == .orderedSame {
+            entrarButton.isEnabled = true
+        } else {
+            passwordTextField.layer.borderColor = UIColor.red.cgColor
+            passwordTextField.layer.borderWidth = 2
+            confirmPasswordTextField.layer.borderColor = UIColor.red.cgColor
+            confirmPasswordTextField.layer.borderWidth = 2
+            entrarButton.isEnabled = false
+        }
+                
         if nameTextField.text?.isEmpty == false && enderecoTextField.text?.isEmpty == false && passwordTextField.text?.isEmpty == false && confirmPasswordTextField.text?.isEmpty == false {
             entrarButton.isEnabled = true
         } else {
@@ -72,7 +95,7 @@ extension ViewController: UITextFieldDelegate {
             textField.layer.borderWidth = 2
             textField.layer.borderColor = UIColor.red.cgColor
         } else {
-            textField.layer.borderWidth = 2
+            textField.layer.borderWidth = 1
             textField.layer.borderColor = UIColor.lightGray.cgColor
         }
     }
