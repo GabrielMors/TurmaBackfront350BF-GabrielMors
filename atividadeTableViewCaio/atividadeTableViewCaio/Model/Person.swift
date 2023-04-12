86d8953b11edfbb1c30d27fdd107df78ea86e02e
Gabriel Mors <gabrielmors210@gmail.com>
2023-04-11 22:22:49 -0400

    tableview feita com sucesso

---
+++ atividadeTableViewCaio/atividadeTableViewCaio/Model/Person.swift
@@ -0,0 +1,14 @@
+//
+//  Person.swift
+//  atividadeTableViewCaio
+//
+//  Created by Gabriel Mors  on 11/04/23.
+//
+
+import Foundation
+
+
+struct Person {
+    var nameList: String
+    var imageListe: String
+}
