0f5ca8ef0c9c38b4814d327f54789d3608ce072d
Gabriel Mors <gabrielmors210@gmail.com>
2023-04-11 21:13:14 -0400

    fazendo algumas anotacoes

--- primeiroAppTableView/primeiroAppTableView/Cell/NameTableViewCell.swift
+++ primeiroAppTableView/primeiroAppTableView/Cell/NameTableViewCell.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
-
+//MARK: O IDENTIFIER deve conter o mesmo nome do file
 class NameTableViewCell: UITableViewCell {
 
     @IBOutlet weak var nameLabel: UILabel!
     
+    static let identifier = "NameTableViewCell"
+    static func nib() -> UINib {
+        return UINib(nibName: self.identifier, bundle: nil)
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
     
 }
