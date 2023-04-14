9af4800fd0356f183f4911aaf909ac3ad9e49147
Gabriel Mors <gabrielmors210@gmail.com>
2023-04-13 23:42:09 -0400

    fiz arquivo para tabkeViewAvancado

---
+++ tableViewAvancado/tableViewAvancado/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableViewAvancado
+//
+//  Created by Gabriel Mors  on 13/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
