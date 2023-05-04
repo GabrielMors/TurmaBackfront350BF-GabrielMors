73f417fc74dae2dc601202a80d7e8c855eb2a298
Gabriel Mors <gabrielmors210@gmail.com>
2023-05-04 18:43:47 -0400

    coloquei meus elementos na tela 1 e adicionei file para a tela 2

---
+++ notificationCenterEProtocol/notificationCenterEProtocol/Tela02/Tela02.swift
@@ -0,0 +1,19 @@
+//
+//  Tela02.swift
+//  notificationCenterEProtocol
+//
+//  Created by Gabriel Mors  on 04/05/23.
+//
+
+import UIKit
+
+class Tela02: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
