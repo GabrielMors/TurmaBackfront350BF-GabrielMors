ab2370e88cd2cf0d376db398d420fe32b2e78070
Gabriel Mors <gabrielmors210@gmail.com>
2023-05-25 18:43:16 -0400

    [FIX] configurei meu projeto para o viewCode

---
+++ ChatGPT/ChatGPT/Screen/HomeScreen.swift
@@ -0,0 +1,21 @@
+//
+//  HomeScreen.swift
+//  ChatGPT
+//
+//  Created by Gabriel Mors  on 25/05/23.
+//
+
+import UIKit
+
+class HomeScreen: UIView {
+
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
---
+++ ChatGPT/ChatGPT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatGPT
+//
+//  Created by Gabriel Mors  on 25/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
