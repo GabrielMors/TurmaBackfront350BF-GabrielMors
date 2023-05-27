2ef399887510dd70374e2b71d82d91fae7c6f906
Gabriel Mors <gabrielmors210@gmail.com>
2023-05-26 22:39:40 -0400

    criando meus elementos de visualizacao

--- ChatGPT/ChatGPT/Screen/HomeScreen.swift
+++ ChatGPT/ChatGPT/Screen/HomeScreen.swift
@@ -9,6 +9,39 @@ import UIKit
 
 class HomeScreen: UIView {
 
+    lazy var tableViewChat: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.showsVerticalScrollIndicator = false
+        tableView.isScrollEnabled = true
+        
+        return tableView
+    }()
+    
+    lazy var contentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        
+        return view
+    }()
+    
+    lazy var subContentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .blue
+        
+        return view
+    }()
+    
+    lazy var sendButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "paperplane.fill"), for: .normal)
+        button.isEnabled = false
+        
+        return button
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -18,4 +51,24 @@ class HomeScreen: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func addSubViews() {
+        addSubview(tableViewChat)
+        addSubview(contentView)
+        addSubview(subContentView)
+    }
+    
+    public func configTableViewProtocol(delegate: UITableViewDelegate, dataSource: UITableViewDataSource) {
+        tableViewChat.delegate = delegate
+        tableViewChat.dataSource = dataSource
+    }
+    
+    private func configConstraints() {
+        NSLayoutConstraint.activate([
+        
+            tableViewChat.
+        
+        
+        
+        ])
+    }
 }
