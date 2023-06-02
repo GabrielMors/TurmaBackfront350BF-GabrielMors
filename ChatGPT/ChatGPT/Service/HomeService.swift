56e4432cc76e795c2f7d399f74d4fc7718975e12
Gabriel Mors <gabrielmors210@gmail.com>
2023-06-01 22:13:17 -0400

    criando um file para meu service

---
+++ ChatGPT/ChatGPT/Service/HomeService.swift
@@ -0,0 +1,17 @@
+//
+//  HomeService.swift
+//  ChatGPT
+//
+//  Created by Gabriel Mors  on 01/06/23.
+//
+
+import UIKit
+import OpenAISwift
+
+class HomeService {
+    
+    func sendMessage(text: String, completion: @escaping (Result<String, OpenAIError>) -> Void) {
+        
+    }
+    
+}
